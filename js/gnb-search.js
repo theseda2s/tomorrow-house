4102bcd6311d70de4529aae458d70aaafcba4479
theseda2s <theseda2s@naver.com>
2022-07-08 16:50:28 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -2,6 +2,11 @@ const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
     gnbSearchHistory.classList.remove('is-active')
@@ -10,6 +15,10 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
@@ -17,3 +26,10 @@ function openGnbSearchHistory() {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistoryItems() {
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
