5e5859e5d1e05a7598038f65e767a6c10e5c12da
theseda2s <theseda2s@naver.com>
2022-07-12 18:49:23 +0900

    🚧 Implement checkout-items when selecting a product and delete it by clicking delete-button

--- js/order-cta.js
+++ js/order-cta.js
@@ -4,6 +4,9 @@ const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
 const orderModal = document.querySelector('.order-form-modal')
 const orderOverlay = document.querySelector('.overlay')
 
+const formSelectList = orderModal.querySelectorAll('.form-select')
+const checkoutList = orderModal.querySelector('.checkout-list')
+
 const bookmarkToast = document.querySelector('.bookmark-toast')
 
 function openOrderModal() {
@@ -54,4 +57,64 @@ orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
 
 function closeBookmarkToast() {
   bookmarkToast.classList.remove('is-active')
+}
+
+function addCheckoutItem(e) {
+  const selectOption = this.options[this.selectedIndex].text
+  const [item, price] = selectOption.split('(', 2)
+  const itemPrice = price.slice(0, -2)
+  const checkoutItem = document.createElement('li')
+  checkoutItem.setAttribute('class', 'checkout-item')
+  checkoutItem.innerHTML = `
+                              <div class="checkout-card">
+                              <header class="checkout-header">
+                                <h4 class="checkout-title">${item}</h4>
+                                <button
+                                  class="delete-button"
+                                  type="button"
+                                  aria-label="해당 상품을 삭제하기"
+                                >
+                                  <i class="ic-close" aria-hidden></i>
+                                </button>
+                              </header>
+
+                              <footer class="checkout-footer">
+                                <div class="checkout-select">
+                                  <select id="order-form-modal-checkout-item-1">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                  </select>
+                                  <i class="ic-caret" aria-hidden></i>
+                                </div>
+
+                                <output
+                                  class="checkout-output"
+                                  for="order-form-modal-checkout-item-1"
+                                >
+                                  <div class="price-16">
+                                    <strong class="amount">${itemPrice}</strong>
+                                    <span class="currency">원</span>
+                                  </div>
+                                </output>
+                              </footer>
+                            </div>
+                          `
+  checkoutList.prepend(checkoutItem)
+  e.target.value = ''
+
+  const checkoutDeleteButton = checkoutItem.querySelector('.delete-button')
+
+  checkoutDeleteButton.addEventListener('click', deleteCheckoutItem)
+}
+
+formSelectList.forEach((select) => {
+  select.addEventListener('change', addCheckoutItem)
+})
+
+function deleteCheckoutItem(e) {
+  const deleteItem = e.target.parentNode.parentNode.parentNode.parentNode
+  checkoutList.removeChild(deleteItem)
 }
